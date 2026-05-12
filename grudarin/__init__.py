3ba371b20f87326d5089174ff94dfc8ecb7c680d
Chintan Patel <216989679+Chintanpatel24@users.noreply.github.com>
2026-05-12 22:15:19 +0530

    Update __init__.py

--- grudarin/__init__.py
+++ grudarin/__init__.py
@@ -2,6 +2,6 @@
 Grudarin - Network Monitor + Vulnerability Scanner + Force-Directed Graph
 """
 
-__version__ = "2.1.0"
+__version__ = "1.1.8"
 __author__ = "Grudarin Contributors"
 __license__ = "GPL-3.0 license"
